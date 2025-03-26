70305dc1eb93f2f086f50c74190a77fbd23db3d0
Larisa <lbsmith2@mail.fhsu.edu>
2025-03-26 19:23:18 -0400

    3/26 7:23

--- api/authors/index.php
+++ api/authors/index.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With, Authorized');
     exit();
 }
 switch ($method) {
